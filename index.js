0ab3c9562531b5e06138f01c92ba4cc22234185b
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2022-11-16 23:08:03 +0300

    arithimetic

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+    return "math"
+}
+function multiply(){
+    return "math"
+}
+function subtract(){
+    return "math"
+}
+function divide(){
+    return "math"
+}
+function add( a,b){
+    return a+=b;
+}
+function subtract(a,b){
+    return a-=b;
+}
+function multiply(a,b){
+    return a*=b;
+}
+function divide(a,b){
+    return a/=b;
+}
+function increment(n){
+     return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+parseInt (n)
+function makeInt(n){
+    return n;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
